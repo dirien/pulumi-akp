695f72313d81fff9cc99674525465389b61b96b1
Engin Diri <info@ediri.de>
2024-04-25 07:02:21 +0200

    feat: initial commit (#1)

---
+++ provider/resources.go
@@ -0,0 +1,157 @@
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package akp
+
+import (
+	_ "embed"
+	"fmt"
+	"github.com/dirien/pulumi-akp/provider/pkg/version"
+	tfbridgetokens "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/tokens"
+	"path/filepath"
+
+	"github.com/akuity/terraform-provider-akp/akp"
+	pf "github.com/pulumi/pulumi-terraform-bridge/pf/tfbridge"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
+	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
+)
+
+// all of the token components used below.
+const (
+	// This variable controls the default name of the package in the package
+	// registries for nodejs and python:
+	mainPkg = "akp"
+	// modules:
+	mainMod = "index" // the akp module
+)
+
+//go:embed cmd/pulumi-resource-akp/bridge-metadata.json
+var metadata []byte
+
+// preConfigureCallback is called before the providerConfigure function of the underlying provider.
+// It should validate that the provider can be configured, and provide actionable errors in the case
+// it cannot be. Configuration variables can be read from `vars` using the `stringValue` function -
+// for example `stringValue(vars, "accessKey")`.
+func preConfigureCallback(vars resource.PropertyMap, c shim.ResourceConfig) error {
+	return nil
+}
+
+// Provider returns additional overlaid schema and metadata associated with the provider..
+func Provider() tfbridge.ProviderInfo {
+	// Instantiate the Terraform provider
+	test := akp.New(version.Version)
+
+	// Create a Pulumi provider mapping
+	prov := tfbridge.ProviderInfo{
+		P:    pf.ShimProvider(test()),
+		Name: "akp",
+		// DisplayName is a way to be able to change the casing of the provider
+		// name when being displayed on the Pulumi registry
+		DisplayName: "Akuity Platform",
+		// The default publisher for all packages is Pulumi.
+		// Change this to your personal name (or a company name) that you
+		// would like to be shown in the Pulumi Registry if this package is published
+		// there.
+		Publisher: "dirien",
+		// LogoURL is optional but useful to help identify your package in the Pulumi Registry
+		// if this package is published there.
+		//
+		// You may host a logo on a domain you control or add an SVG logo for your package
+		// in your repository and use the raw content URL for that file as your logo URL.
+		LogoURL: "",
+		// PluginDownloadURL is an optional URL used to download the Provider
+		// for use in Pulumi programs
+		// e.g https://github.com/org/pulumi-provider-name/releases/
+		PluginDownloadURL: "github://api.github.com/dirien/pulumi-akp",
+		Description:       "A Pulumi package for creating and managing Akuity Platform cloud resources.",
+		// category/cloud tag helps with categorizing the package in the Pulumi Registry.
+		// For all available categories, see `Keywords` in
+		// https://www.pulumi.com/docs/guides/pulumi-packages/schema/#package.
+		Keywords:     []string{"pulumi", "akp", "category/utility"},
+		License:      "Apache-2.0",
+		Homepage:     "https://www.pulumi.com",
+		MetadataInfo: tfbridge.NewProviderMetadata(metadata),
+		Repository:   "https://github.com/dirien/pulumi-akp",
+		// The GitHub Org for the provider - defaults to `terraform-providers`. Note that this
+		// should match the TF provider module's require directive, not any replace directives.
+		GitHubOrg: "akuity",
+		Config: map[string]*tfbridge.SchemaInfo{
+			"api_key_id": {
+				Default: &tfbridge.DefaultInfo{
+					EnvVars: []string{"AKUITY_API_KEY_ID"},
+				},
+				Secret: tfbridge.BoolRef(true),
+			},
+			"api_key_secret": {
+				Default: &tfbridge.DefaultInfo{
+					EnvVars: []string{"AKUITY_API_KEY_SECRET"},
+				},
+				Secret: tfbridge.BoolRef(true),
+			},
+			"server_url": {
+				Default: &tfbridge.DefaultInfo{
+					EnvVars: []string{"AKUITY_SERVER_URL"},
+				},
+			},
+		},
+		PreConfigureCallback: preConfigureCallback,
+		Resources:            map[string]*tfbridge.ResourceInfo{},
+		DataSources:          map[string]*tfbridge.DataSourceInfo{},
+		JavaScript: &tfbridge.JavaScriptInfo{
+			PackageName: "@ediri/akp",
+			// List any npm dependencies and their versions
+			Dependencies: map[string]string{
+				"@pulumi/pulumi": "^3.0.0",
+			},
+			DevDependencies: map[string]string{
+				"@types/node": "^10.0.0", // so we can access strongly typed node definitions.
+				"@types/mime": "^2.0.0",
+			},
+			// See the documentation for tfbridge.OverlayInfo for how to lay out this
+			// section, or refer to the AWS provider. Delete this section if there are
+			// no overlay files.
+			//Overlay: &tfbridge.OverlayInfo{},
+		},
+		Python: &tfbridge.PythonInfo{
+			PackageName: "ediri_akp",
+			// List any Python dependencies and their version ranges
+			Requires: map[string]string{
+				"pulumi": ">=3.0.0,<4.0.0",
+			},
+		},
+		Golang: &tfbridge.GolangInfo{
+			ImportBasePath: filepath.Join(
+				fmt.Sprintf("github.com/dirien/pulumi-%[1]s/sdk/", mainPkg),
+				tfbridge.GetModuleMajorVersion(version.Version),
+				"go",
+				mainPkg,
+			),
+			GenerateResourceContainerTypes: true,
+		},
+		CSharp: &tfbridge.CSharpInfo{
+			RootNamespace: "ediri",
+			PackageReferences: map[string]string{
+				"Pulumi": "3.*",
+			},
+		},
+	}
+
+	prov.MustComputeTokens(tfbridgetokens.SingleModule("akp_", mainMod,
+		tfbridgetokens.MakeStandard(mainPkg)))
+
+	prov.SetAutonaming(255, "-")
+
+	return prov
+}
